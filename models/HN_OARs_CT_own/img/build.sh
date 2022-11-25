261cd2a574acb45bb010674e6766a2e3b0339fc4
Mathis Rasmussen <mathis@oncology.au.dk>
2022-11-25 08:51:15 +0100

    cleaning up

--- models/HN_OARs_CT_own/img/build.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t mathiser/inference_server_models:HN_OAR_CT_DCPT
-docker push mathiser/inference_server_models:HN_OAR_CT_DCPT
--- models/csi_ct_oars/img/build.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t mathiser/inference_server_models:CNS_JESKAL_504_DCPTATLAS_merged
-docker push mathiser/inference_server_models:CNS_JESKAL_504_DCPTATLAS_merged
